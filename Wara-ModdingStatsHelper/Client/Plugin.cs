906ee400c808d7663dcc333751bd5d57ec868795
Soulztorm <FRL@gmx.de>
2024-04-11 01:35:01 +0200

    Add project files.

---
+++ Wara-ModdingStatsHelper/Client/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using BepInEx;
+using UnityEngine;
+
+namespace ShowMeTheStats
+{
+    [BepInPlugin("com.moddingstatshelper.aki", "Modding Stats Helper", "1.0")]
+    public class ShowMeTheStatsPlugin : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            new ItemShowTooltipPatch().Enable();
+            new ShowTooltipPatch().Enable();
+            new WeaponUpdatePatch().Enable();
+            new DropDownSlotContextPatch().Enable();
+            new SlotViewPatch().Enable();
+            new ScreenTypePatch().Enable();
+
+            new DropDownSlotContextClosePatch().Enable();
+        }
+
+
+
+        void Update()
+        {
+            if (Globals.isWeaponModding)
+            {
+                bool isKeyDown = Input.GetKey(KeyCode.LeftControl);
+
+                if (isKeyDown && !Globals.isKeyPressed)
+                {
+                    Globals.isKeyPressed = true;
+                    Globals.simpleTooltip.Show("", null, 0.1f, null);
+                }
+                else if (!isKeyDown && Globals.isKeyPressed)
+                {
+                    Globals.isKeyPressed = false;
+                    Globals.simpleTooltip.Show("", null, 0.1f, null);
+                }
+            }
+        }
+    }
+}
---
+++ Wara-ModdingStatsHelper/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.3.0")]
+[assembly: AssemblyCompany("wara")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
+[assembly: AssemblyProduct("Wara-ModdingStatsHelper")]
+[assembly: AssemblyTitle("Wara-ModdingStatsHelper")]
