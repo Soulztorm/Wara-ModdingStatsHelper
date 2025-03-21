c65c84c14a4715b38c43cd275556ddb175453e79
Soulztorm <FRL@gmx.de>
2025-03-21 23:13:20 +0100

    Update version number for SPT 3.11

--- Wara-ModdingStatsHelper/Properties/AssemblyInfo.cs
+++ Wara-ModdingStatsHelper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.0.5.0")]
+[assembly: AssemblyVersion("1.0.6.0")]
 [assembly: AssemblyCompany("wara")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.5")]
-[assembly: AssemblyInformationalVersion("1.0.5")]
+[assembly: AssemblyFileVersion("1.0.6")]
+[assembly: AssemblyInformationalVersion("1.0.6")]
 [assembly: AssemblyProduct("Wara-ModdingStatsHelper")]
 [assembly: AssemblyTitle("Wara-ModdingStatsHelper")]
