9803ec093aba84b83bc02d61cdeab74bdc93983d
Soulztorm <FRL@gmx.de>
2025-10-26 19:16:05 +0100

    Update to SPT 4.0

--- Wara-ModdingStatsHelper/Properties/AssemblyInfo.cs
+++ Wara-ModdingStatsHelper/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.Versioning;
 
-[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyVersion("1.1.0")]
 [assembly: AssemblyCompany("wara")]
 [assembly: AssemblyConfiguration("Release")]
-[assembly: AssemblyFileVersion("1.0.6")]
-[assembly: AssemblyInformationalVersion("1.0.6")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 [assembly: AssemblyProduct("Wara-ModdingStatsHelper")]
 [assembly: AssemblyTitle("Wara-ModdingStatsHelper")]
